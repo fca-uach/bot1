3cc9b9800fd1dff5bff2ee213340e78cac3135c7
fca-uach <fca.uach@gmail.com>
2024-11-23 13:43:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 
-
+st.balloons()
 # Show title and description.
 st.title("💬 La Cybermoneda Tibetana ´Onlain´")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+   "Pregúntame lo que quieras saber, solo aguanta las respuestas... "
    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
