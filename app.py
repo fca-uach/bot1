d3387bb9802ac26cf7d91768eb5843792b127950
fca-uach <fca.uach@gmail.com>
2024-11-23 13:53:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.balloons()
 # Show title and description.
 st.title("💬 La Cybermoneda Tibetana ´Onlain´")
 st.write(
-   "Pregúntame lo que quieras saber, solo aguanta...  "
+   "Pregúntame lo que quieras saber... Solo aguanta.    "
    "Las respuestas que te proporciono se basan en las milenarias y místicas enseñanzas de los barrios bajos de Chihuahua.   "
-   "Queda estrictamente prohibido agredir a tu pareja, cónyugue o lonche por consecuencia de las respuestas. "
+   "Queda estrictamente prohibido agredir a tu pareja, cónyugue o lonche por consecuencia de las respuestas... "
 )
 openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
